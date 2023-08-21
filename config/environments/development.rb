84e1019072126377bd414aac13ba2ea5f8d6eae4
Erik Mota Galindo <kenshimota@gmail.com>
2023-08-20 21:23:34 -0300

    add information to deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+
+  hostname = ENV["HOSTNAME_APP"] || nil
+
+  if hostname
+    config.hosts << hostname
+  end
+
+
 end
