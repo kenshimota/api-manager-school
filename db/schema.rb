eb3d64e77b8995c5ca64d6a20c1b5d158f1aacc2
Erik Mota Galindo <kenshimota@gmail.com>
2023-08-25 20:32:28 -0300

    add model role to users that they will be to create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_005838) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_231536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_005838) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["identity_document"], name: "index_people_on_identity_document", unique: true
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_roles_on_name", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+["student", "manager", "representative", "profesor"].each do |name|
+  Role.find_by_or_create(name: name)
+end
