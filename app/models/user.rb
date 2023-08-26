bbdebbc743c429396019f8f4b7f8f7842f55f8ef
Erik Mota Galindo <kenshimota@gmail.com>
2023-08-25 22:57:42 -0300

    create model user and test unit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  belongs_to :role
+  belongs_to :person
+  validates :password, presence: true, format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.]).{8,}/ }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
