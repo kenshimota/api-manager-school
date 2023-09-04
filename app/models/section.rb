aa7610fec70f9e436294781c4668365cb9210eaa
Erik Mota Galindo <kenshimota@gmail.com>
2023-09-04 00:15:47 -0300

    apply validations on section model

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,7 @@
 class Section < ApplicationRecord
+  before_create do
+    self.letter = self.letter.upcase
+  end
+
+  validates :letter, presence: true, length: { maximum: 1, minimum: 1 }, uniqueness: { case_sensitive: false }
 end
