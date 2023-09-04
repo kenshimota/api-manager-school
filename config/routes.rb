aa72e32a0a97c1d9eaecc2213b80b7bdd9c04262
Erik Mota Galindo <kenshimota@gmail.com>
2023-09-03 23:49:20 -0300

    generate resource sections

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope :api, :defaults => { :format => "json" } do
     resources :students
     resources :subjects
+    resources :sections
     devise_for :users, { controllers: { sessions: "users/sessions" } }
   end
 end
