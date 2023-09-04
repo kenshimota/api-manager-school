47e8ff786a77f412d1cf30d5f3903f0af10e06ad
Erik Mota Galindo <kenshimota@gmail.com>
2023-09-04 00:15:05 -0300

    create unit test about section model

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,5 +1,29 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Section, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validate create a section" do
+    it "a section doesn't be void" do
+      expect(Section.new).to_not be_valid
+    end
+
+    it "a section doesn't be letter void" do
+      expect(Section.create(letter: nil)).to_not be_valid
+      expect(Section.create(letter: "")).to_not be_valid
+    end
+
+    it "a section doesn't have string with greater two letter" do
+      name_not_valid = "name-section-1"
+      expect(Section.create(letter: name_not_valid)).to_not be_valid
+    end
+
+    it "save section unique and convert string lowercase to upcase" do
+      name_valid = "a"
+      section = Section.create(letter: name_valid)
+
+      expect(section).to be_valid
+      expect(section.letter).to eq(name_valid.upcase)
+      expect(Section.create(letter: name_valid)).to_not be_valid
+      expect(Section.create(letter: name_valid.upcase)).to_not be_valid
+    end
+  end
 end
