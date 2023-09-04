bbbe8848eb1f0e4327f71beefd0860ec5581dd16
Erik Mota Galindo <kenshimota@gmail.com>
2023-09-04 00:47:53 -0300

    becoming letter from lowercase to upcase on section model

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,5 +3,9 @@ class Section < ApplicationRecord
     self.letter = self.letter.upcase
   end
 
+  before_update do
+    self.letter = self.letter.upcase
+  end
+
   validates :letter, presence: true, length: { maximum: 1, minimum: 1 }, uniqueness: { case_sensitive: false }
 end
